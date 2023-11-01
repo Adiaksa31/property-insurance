c477893797e7e79bf9b4d1e13dc1478cc6004d60
Adiaksa31 <programerbiasa.com>
2023-11-01 16:09:02 +0800

    update halaman dropdown dan responsive

--- js/custom.js
+++ js/custom.js
@@ -370,3 +370,13 @@ $(function () {
 
 
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const accordionItems = document.querySelectorAll('.accordion-item');
+    
+    accordionItems.forEach(function(item) {
+      item.addEventListener('click', function() {
+        item.classList.toggle('active');
+      });
+    });
+  });
